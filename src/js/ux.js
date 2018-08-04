8ae1df3b6343116dd652d173f8445004ac7f87d4
Stephan Johnson <stephan@Stephans-MacBook-Pro.local>
2018-08-04 10:39:36 +0200

    render functionally complete

--- src/js/ux.js
+++ src/js/ux.js
@@ -23,6 +23,33 @@ function render(canvas, data) {
     }
 
     // RENDER THE DATA
+    var width = canvas.width;
+    var height = canvas.height;
+    var blockWidth = (width * 0.8) / data.size.x;
+    var blockHeight = (height * 0.8) / data.size.y;
+    var xOffset = width * 0.1;
+    var yOffset = height * 0.1;
 
-    
+    // make blocks slightly smaller
+    blockWidth -= 5;
+    blockHeight -= 5;
+
+    var context = canvas.getContext('2d');
+
+    for (var y = 0; y < data.size.y; y++){
+        for (var x = 0; x < data.size.x; x++){
+            context.fillStyle = getFillStyle(grid[y][x]);
+            context.fillRect((x * blockWidth) + xOffset, (y * blockHeight) + yOffset, blockWidth, blockHeight);
+        }
+    }
+
+}
+
+function getFillStyle(cell){
+    if (cell == 1){
+        return 'rgb(0, 0, 0)';
+    }
+    else{
+        return 'rgb(255, 255, 255)';
+    }
 }
